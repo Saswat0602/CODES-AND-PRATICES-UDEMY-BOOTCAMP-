c3b6c3b87f8a8768dfff78376d65ad1210780ae6
saswat <saswatranjan0602@gmail.com>
2023-07-01 16:33:16 +0530

    scorekeeper

--- React/state-app/src/App.jsx
+++ React/state-app/src/App.jsx
@@ -3,17 +3,17 @@ import { useState } from 'react'
 import './App.css'
 import Counter from './Counter'
 import Dum from './Dum'
-
+import ScoreKeeper from './ScoreKeeper'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Counter/>
-    <Dum/>
-     
+      {/* <Counter /> */}
+      {/* <Dum /> */}
+      <ScoreKeeper />
     </>
-  )
+  );
 }
 
 export default App
