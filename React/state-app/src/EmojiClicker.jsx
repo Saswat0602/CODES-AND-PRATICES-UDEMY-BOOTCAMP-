f6d2a1a4ee9aef23c4a8d7681855bdf600cb46dc
saswat <saswatranjan0602@gmail.com>
2023-07-01 17:14:42 +0530

    emoji

--- React/state-app/src/EmojiClicker.jsx
+++ React/state-app/src/EmojiClicker.jsx
@@ -1,16 +1,30 @@
 import React, { useState } from "react";
 import {v4 as uuid} from "uuid"
 
+function randomEmoji() {
+  const choices = ["😬", "😘", "😭", "🙄", "🤪", "🤢","😒","🤦‍♂️","😎"];
+  return choices[Math.floor(Math.random() * choices.length)];
+}
+
+
+
 const EmojiClicker = () => {
   const [emojis, sertEmojis] = useState([{ id:uuid(),emoji:"⭐"}]);
   const addEmoji = () => {
-    sertEmojis((oldEmojis) => [...oldEmojis, { id: uuid(), emoji: "😁" }]);
+    sertEmojis((oldEmojis) => [...oldEmojis, { id: uuid(), emoji: randomEmoji()  }]);
   };
+const deleteEmoji = (id) => {
+  sertEmojis((emg) => {
+    return emg.filter((e) => e.id !== id);
+  });
+};
 
   return (
     <div>
       {emojis.map((e) => (
-        <span key={e.id} >{e.emoji}</span>
+        <span onClick={() => deleteEmoji(e.id)} key={e.id}>
+          {e.emoji}
+        </span>
       ))}
       <button onClick={addEmoji}>Add Emoji</button>
     </div>
