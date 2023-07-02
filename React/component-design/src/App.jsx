80d947464d086dc0496cacd36c09b15d24e64569
saswat <saswatranjan0602@gmail.com>
2023-07-02 09:58:16 +0530

    lucky7

---
+++ React/component-design/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Lucky7 from './Lucky7'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <Lucky7 />
+    </>
+  );
+}
+
+export default App
