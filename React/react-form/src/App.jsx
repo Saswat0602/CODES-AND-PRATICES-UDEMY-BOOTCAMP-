357d8f1064838406646392983fcd98172e1f2622
saswat <saswatranjan0602@gmail.com>
2023-07-03 12:46:49 +0530

    form

--- React/react-form/src/App.jsx
+++ React/react-form/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <>
       {/* <UserNameForm /> */}
       {/* <Signup2 /> */}
-      <ShopingListFom />
+      {/* <ShopingListFom /> */}
       <ShpoingList />
     </>
   );
