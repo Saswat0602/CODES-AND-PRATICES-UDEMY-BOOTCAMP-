61947d57ac41e5d786525092b5a346fe8a077a78
saswat <saswatranjan0602@gmail.com>
2023-07-03 09:38:33 +0530

    signup

--- React/react-form/src/App.jsx
+++ React/react-form/src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
 import UserNameForm from "./UserNameForm";
 import SignUpForm from "./SignUpForm";
+import Signup2 from "./SignUp2";
 
 import "./App.css";
 
 function App() {
   return (
     <>
-      <UserNameForm />
-      <SignUpForm />
+      {/* <UserNameForm /> */}
+      <Signup2 />
     </>
   );
 }
