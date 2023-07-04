67bc618664cc80ca8481720df63db20a0aa5dc2b
saswat <saswatranjan0602@gmail.com>
2023-07-04 10:23:10 +0530

    counter

--- React/react-effects/src/Counter.jsx
+++ React/react-effects/src/Counter.jsx
@@ -1,30 +1,29 @@
-import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from "react";
 
-const Counter = () => {
-    const [count,setCount ] = useState(0)
-      const [name, setName] = useState("");
-      
-      useEffect(function effects() {
-        console.log("use effect");
-      }, []);
+export default function Counter() {
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
 
+  useEffect(
+    function myEffect() {
+      console.log("MY EFFECT WAS CALLED!");
+    },
+    [count]
+  );
 
+  const increment = () => {
+    setCount((c) => c + 1);
+  };
+  const handleChange = (e) => {
+    setName(e.target.value);
+  };
 
-    const increment =()=>{
-        setCount((c)=> c+1)
-    }
-    const handelChange = (e)=>{
-      setName(e.target.value)
-    }
   return (
     <div>
       <h1>{count}</h1>
       <button onClick={increment}>+1</button>
-      <h3>{name}</h3>
-      <input type="text" onChange={handelChange} value={name} />
+      <p>Name: {name}</p>
+      <input value={name} onChange={handleChange} type="text" />
     </div>
   );
 }
-
-export default Counter
