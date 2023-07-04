9bde3f04d907e4add7db4d13e86a14bb7298ec08
saswat <saswatranjan0602@gmail.com>
2023-07-04 12:13:23 +0530

    mui

--- React/material-ui/src/App.jsx
+++ React/material-ui/src/App.jsx
@@ -1,13 +1,28 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Button from "@mui/material/Button";
+import { IconButton } from "@mui/material";
+
+import AlarmIcon from "@mui/icons-material/Alarm";
 
 function App() {
-
   return (
     <>
-    <h1>hhh</h1>
+      <div>
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+        <Button color="error" variant="outlined">
+          Outlined
+        </Button>
+      </div>
+      <div>
+        <IconButton color="secondary" aria-label="add an alarm">
+          <AlarmIcon />
+        </IconButton>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
